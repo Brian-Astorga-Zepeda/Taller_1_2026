636550171791e9d4c962e7195e5dd5db59578a70
Prissyla Gomez <prissyla.gomez@alumnos.ucn.cl>
2026-04-27 21:57:14 -0400

    se agregan funciones a LinkedList.h

--- data_structures/LinkedList.h
+++ data_structures/LinkedList.h
@@ -15,7 +15,7 @@ public:
     Node<T>* getHead() const { return head;}
     Node<T>* getTail() const { return tail;}
     int getSize() const { return size;}
-
+    bool empty() const { return size == 0; }
 
     T& front() { 
         return head->data;
@@ -25,24 +25,41 @@ public:
         return tail->data;
     }
    
-   
+    void clear() {
+        while (head) popFront();
+    }
    
     void pushBack(T val) {
-
+        Node<T>* n = new Node<T>(val);
+        if (!tail) { head = tail = n; }
+        else { tail->next = n; n->prev = tail; tail = n; }
+        size++;
     }
 
     void pushFront(T val) {
-
+        Node<T>* n = new Node<T>(val);
+        if (!head) { head = tail = n; }
+        else { n->next = head; head->prev = n; head = n; }
+        size++;
     }
     
     void popFront() {
-
+        if (!head) return;
+        Node<T>* tmp = head;
+        head = head->next;
+        if (head) head->prev = nullptr;
+        else tail = nullptr;
+        delete tmp;
+        size--;
     }
 
     void popBack() {
-
+        if (!tail) return;
+        Node<T>* tmp = tail;
+        tail = tail->prev;
+        if (tail) tail->next = nullptr;
+        else head = nullptr;
+        delete tmp;
+        size--;
     }
-
-
-    
 };
